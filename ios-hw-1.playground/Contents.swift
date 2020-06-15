28c28872367be7f86c13f6922a8108c05cb39e28
Shaha Alajeel <saalajeel@gmail.com>
2020-06-15 06:13:18 +0300

    ios-hw-1-completed

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -5,12 +5,12 @@
  
  */
 
-let name = "Ahmad"
+let name1 = "Ahmad"
 
-name = "Salem"
+let name2 = "Salem"
 
 
-var myWeight = 60
+var myWeight = 60.0
 var FriendWeight = 80.5
 var height = 1.5
 
@@ -38,28 +38,39 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "Amelia"
+var lastName1 = "Earhast"
+var email1 = "ameliaearhart101@gmail.com"
+var phoneNumber1 = "76364966"
+var age1 = 39
+var country1 = "USA"
+var area1 = "Atchison"
+var password1 = "ameliaeee"
+var isKuwaiti1 = false
 
 //Person 2
-
-
-
-
-
-
-
+var firstName2 = "Petula"
+var lastName2 = "De Wilde"
+var email2 = "petulaaaaa@gmail.com"
+var phoneNumber2 = "64738320"
+var age2 = 15
+var country2 = "USA"
+var area2 = "Pittsburgh"
+var password2 = "catscatscatsandmovies"
+var isKuwaiti2 = false
 
 
 
 //Person 3
+var firstName3 = "Dwight"
+var lastName3 = "Schrute"
+var email3 = "assistantregionalmanager"
+var phoneNumber3 = "45838106"
+var age3 = 52
+var country3 = "USA"
+var area3 = "Scranton"
+var password3 = "000000"
+var isKuwaiti3 = false
 
 
 
@@ -82,3 +93,205 @@ var isKuwaiti = false
 
  
  */
+firstName1 != firstName2
+firstName1 == firstName2
+firstName1 > firstName2
+firstName1 < firstName2
+firstName1 >= firstName2
+firstName1 <= firstName2
+
+firstName1 != firstName3
+firstName1 == firstName3
+firstName1 > firstName3
+firstName1 < firstName3
+firstName1 >= firstName3
+firstName1 <= firstName3
+
+firstName2 != firstName3
+firstName2 == firstName3
+firstName2  > firstName3
+firstName2  < firstName3
+firstName2 >= firstName3
+firstName2 <= firstName3
+
+lastName1 != lastName2
+lastName1 == lastName2
+lastName1 > lastName2
+lastName1 < lastName2
+lastName1 >= lastName2
+lastName1 <= lastName2
+
+lastName1 != lastName3
+lastName1 == lastName3
+lastName1 > lastName3
+lastName1 < lastName3
+lastName1 >= lastName3
+lastName1 <= lastName3
+
+lastName2 != lastName3
+lastName2 == lastName3
+lastName2 > lastName3
+lastName2 < lastName3
+lastName2 >= lastName3
+lastName2 <= lastName3
+
+email1 != email2
+email1 == email2
+email1 > email2
+email1 < email2
+email1 >= email2
+email1 <= email2
+
+email1 != email3
+email1 == email3
+email1 > email3
+email1 < email3
+email1 >= email3
+email1 <= email3
+
+email2 != email3
+email2 == email3
+email2 > email3
+email2 < email3
+email2 >= email3
+email2 <= email3
+
+phoneNumber1 != phoneNumber2
+phoneNumber1 == phoneNumber2
+phoneNumber1 > phoneNumber2
+phoneNumber1 < phoneNumber2
+phoneNumber1 >= phoneNumber2
+phoneNumber1 <= phoneNumber2
+
+phoneNumber1 != phoneNumber3
+phoneNumber1 == phoneNumber3
+phoneNumber1 > phoneNumber3
+phoneNumber1 < phoneNumber3
+phoneNumber1 >= phoneNumber3
+phoneNumber1 <= phoneNumber3
+
+phoneNumber2 != phoneNumber3
+phoneNumber2 == phoneNumber3
+phoneNumber2 > phoneNumber3
+phoneNumber2 < phoneNumber3
+phoneNumber2 >= phoneNumber3
+phoneNumber2 <= phoneNumber3
+
+age1 != age2
+age1 == age2
+age1 > age2
+age1 < age2
+age1 >= age2
+age1 <= age2
+
+age1 != age3
+age1 == age3
+age1 > age3
+age1 < age3
+age1 >= age3
+age1 <= age3
+
+age2 != age3
+age2 == age3
+age2 > age3
+age2 < age3
+age2 >= age3
+age2 <= age3
+
+country1 != country2
+country1 == country2
+country1 > country2
+country1 < country2
+country1 >= country2
+country1 <= country2
+
+country1 != country3
+country1 == country3
+country1 > country3
+country1 < country3
+country1 >= country3
+country1 <= country3
+
+country2 != country3
+country2 == country3
+country2 > country3
+country2 < country3
+country2 >= country3
+country2 <= country3
+
+area1 != area2
+area1 == area2
+area1 > area2
+area1 < area2
+area1 >= area2
+area1 <= area2
+
+area1 != area3
+area1 == area3
+area1 > area3
+area1 < area3
+area1 >= area3
+area1 <= area3
+
+area2 != area3
+area2 == area3
+area2 > area3
+area2 < area3
+area2 >= area3
+area2 <= area3
+
+password1 != password2
+password1 == password2
+password1 > password2
+password1 < password2
+password1 >= password2
+password1 <= password2
+
+password1 != password3
+password1 == password3
+password1 > password3
+password1 < password3
+password1 >= password3
+password1 <= password3
+
+password2 != password3
+password2 == password3
+password2 > password3
+password2 < password3
+password2 >= password3
+password2 <= password3
+
+isKuwaiti1 != isKuwaiti2
+isKuwaiti1 == isKuwaiti2
+isKuwaiti1 && isKuwaiti2
+isKuwaiti1 || isKuwaiti2
+
+isKuwaiti1 != isKuwaiti3
+isKuwaiti1 == isKuwaiti3
+isKuwaiti1 && isKuwaiti3
+isKuwaiti1 || isKuwaiti3
+
+isKuwaiti2 != isKuwaiti3
+isKuwaiti2 == isKuwaiti3
+isKuwaiti2 && isKuwaiti3
+isKuwaiti2 || isKuwaiti3
+
+!isKuwaiti1
+!isKuwaiti2
+!isKuwaiti3
+
+age1 > 18
+age2 > 18
+age3 > 18
+
+age1 < 18
+age2 < 18
+age3 < 18
+
+age1 == 17
+age2 == 17
+age3 == 17
+
+age1 != 17
+age2 != 17
+age3 != 17
